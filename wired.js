81cf374c321a7716768b9f32faa9631f5db88ba4
200427063 <58613776+200427063@users.noreply.github.com>
2020-03-25 17:02:46 -0400

    Add files via upload

--- wired.js
+++ wired.js
@@ -1,51 +1,51 @@
 
-//
-function loadProducts(url,callback){
-  let request = new XMLHttpRequest();
-  request.open('GET', url);
-  request.responseType = 'json';
-  request.send();
-  request.onload = function() {
-  callback(request.response);
+function loadProducts(url, callback) {
+    let request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.responseType = 'json';
+    request.send();
+    request.onload = function() {
+        callback(request.response);
 
-  };
+    };
 }
 
 
 //callback function
 
 function producttypes(jsonObj) {
-  let producttypes = jsonObj.producttypes;
+    let producttypes = jsonObj.producttypes;
 
-  let div = document.querySelector('div');
-  for (let i = 0; i< producttypes.length; i++){
-//build the new html element on our page
-    let article = document.createElement('article');
-    let h2 = document.createElement('h2')
-    let img = document.createElement('img')
-    let p1 = document.createElement('p')
-    let p2 = document.createElement('p')
-    let ul = document.createElement('ul')
+    let div = document.querySelector('div');
+    for (let i = 0; i < producttypes.length; i++) {
+        //build the new html element on our page
+        let article = document.createElement('article');
+        let h2 = document.createElement('h2')
+        let img = document.createElement('img')
+        let p1 = document.createElement('p')
+        let p2 = document.createElement('p')
+        let ul = document.createElement('ul')
 
 
-    img.setAttribute('src', 'https://200427063.github.io/Lab4/assets/'
-  + producttypes[i].image);
-  img.setAttribute('alt', producttypes[i].image);
-  h2.textContent = producttypes[i].name;
-  p1.textContent = 'price : ' + producttypes[i].price;
-  p2.textContent = 'details : ' + producttypes[i].details;
-  function myFunction() {
-    p2.document.getElementById("details").innerHTML = + producttypes[i].details;
-  }
+        img.setAttribute('src', 'https://200427063.github.io/Lab4/assets/' +
+            producttypes[i].image);
+        img.setAttribute('alt', producttypes[i].image);
+        h2.textContent = producttypes[i].name;
+        p1.textContent = 'price : ' + producttypes[i].price;
+        p2.textContent = 'details : ' + producttypes[i].details;
 
-  article.appendChild(img);
-    article.appendChild(h2);
-    article.appendChild(p1);
-    article.appendChild(p2);
-    article.appendChild(ul);
-  div .appendChild(article);
+        function myFunction() {
+            p2.document.getElementById("details").innerHTML = +producttypes[i].details;
+        }
 
-  }
+        article.appendChild(img);
+        article.appendChild(h2);
+        article.appendChild(p1);
+        article.appendChild(p2);
+        article.appendChild(ul);
+        div.appendChild(article);
+
+    }
 
 
 }
